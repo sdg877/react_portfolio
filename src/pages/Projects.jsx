812cb1e1b4e8a83ebff85d7cdf6c58d5c1d4f045
Sdg877 <sdrakegill@gmail.com>
2024-11-04 11:25:27 +0000

    Styling for project nav page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,65 +1,98 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import WeatherAppImage from '../Images/WeatherApp.png';
-import SSSImage from '../Images/SSS.png';
-import ProjectOneImage from '../Images/ProjectOne.jpg';
-import SnakeImage from '../Images/Snake.png';
-import InTechImage from '../Images/InTech.png';
-import GourmetGalleryImage from '../Images/GourmetGallery.png';
-import SpookSpottersImage from '../Images/SpookSpotters.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import WeatherAppImage from "../Images/WeatherApp.png";
+import SSSImage from "../Images/SSS.png";
+import ProjectOneImage from "../Images/ProjectOne.jpg";
+import SnakeImage from "../Images/Snake.png";
+import InTechImage from "../Images/InTech.png";
+import GourmetGalleryImage from "../Images/GourmetGallery.png";
+import SpookSpottersImage from "../Images/SpookSpotters.png";
 
 const Projects = () => {
   return (
     <div className="info-card">
-      <h2>Projects</h2>
+      <h3>Projects</h3>
       <h5>Click on an image for more info.</h5>
       <div className="project-container">
         <div className="project-item">
           <h5>Weather App</h5>
           <Link to="/projects/WeatherApp">
-            <img src={WeatherAppImage} alt="Weather App" style={{ width: '200px', height: '200px' }} />
+            <img
+              src={WeatherAppImage}
+              alt="Weather App"
+              className="project-image"
+            />
           </Link>
         </div>
         <div className="project-item">
-          <h5>Confidential MERN Stack Project</h5>
+          <h5>Confidential Project</h5>
           <Link to="/projects/ProjectOne">
-            <img src={ProjectOneImage} alt="Confidential" style={{ width: '200px', height: '200px' }} />
+            <img
+              src={ProjectOneImage}
+              alt="Confidential"
+              className="project-image"
+            />
           </Link>
         </div>
         <div className="project-item">
           <h5>Gourmet Gallery</h5>
           <Link to="/projects/GourmetGallery">
-            <img src={GourmetGalleryImage} alt="Gourmet Gallery" style={{ width: '200px', height: '200px' }} />
+            <img
+              src={GourmetGalleryImage}
+              alt="Gourmet Gallery"
+              className="project-image"
+            />
           </Link>
         </div>
         <div className="project-item">
           <h5>Sustainable Swap Shop</h5>
           <Link to="/projects/SSS">
-            <img src={SSSImage} alt="Sustainable Swap Shop" style={{ width: '200px', height: '200px' }} />
+            <img
+              src={SSSImage}
+              alt="Sustainable Swap Shop"
+              className="project-image"
+            />
           </Link>
         </div>
         <div className="project-item">
-          <h5>inTech</h5>
+          <h5>inTech Jobs</h5>
           <Link to="/projects/InTechJobs">
-            <img src={InTechImage} alt="inTech" style={{ width: '200px', height: '200px' }} />
+            <img
+              src={InTechImage}
+              alt="inTech"
+              className="project-image"
+            />
           </Link>
         </div>
         <div className="project-item">
           <h5>Spook Spotters</h5>
           <Link to="/projects/SpookSpotters">
-            <img src={SpookSpottersImage} alt="Spook Spotters" style={{ width: '200px', height: '200px' }} />
+            <img
+              src={SpookSpottersImage}
+              alt="Spook Spotters"
+              className="project-image"
+            />
           </Link>
         </div>
         <div className="project-item">
           <h5>Snake</h5>
           <Link to="/projects/Snake">
-            <img src={SnakeImage} alt="Snake Project" style={{ width: '200px', height: '200px' }} />
+            <img
+              src={SnakeImage}
+              alt="Snake Project"
+              className="project-image"
+            />
           </Link>
         </div>
       </div>
       <br />
       <p>
-        <a className="nav-button" href="https://github.com/sdg877" target="_blank" rel="noopener noreferrer">
+        <a
+          className="nav-button"
+          href="https://github.com/sdg877"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           For all projects completed, please see my GitHub Page
         </a>
       </p>
