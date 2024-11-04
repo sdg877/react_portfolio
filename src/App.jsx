7a72379ea76443522dbaf6f8718e215363c003a3
Sdg877 <sdrakegill@gmail.com>
2024-11-04 08:55:40 +0000

    Update to remove navbar from home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import NavBar from './components/NavBar.jsx';
-import AboutMe from './Pages/AboutMe.jsx';
-import Education from './Pages/Education.jsx';
-import Home from './Pages/Home.jsx';
-import Projects from './Pages/Projects.jsx';
-import Skills from './Pages/Skills.jsx';
-import ProjectOne from './Pages/ProjectPages/ProjectOne.jsx';
-import InTech from './Pages/ProjectPages/InTech.jsx';
-import SSS from './Pages/ProjectPages/SSS.jsx';
-import WeatherApp from './Pages/ProjectPages/WeatherApp.jsx';
-import GourmetGallery from './Pages/ProjectPages/GourmetGallery.jsx';
-import Snake from './Pages/ProjectPages/Snake.jsx';
-import SpookSpotters from './Pages/ProjectPages/SpookSpotters.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Home from './pages/Home.jsx';
+import Education from './pages/Education.jsx';
+import Projects from './pages/Projects.jsx';
+import Skills from './pages/Skills.jsx';
+import ProjectOne from './pages/ProjectPages/ProjectOne.jsx';
+import InTech from './pages/ProjectPages/InTech.jsx';
+import SSS from './pages/ProjectPages/SSS.jsx';
+import WeatherApp from './pages/ProjectPages/WeatherApp.jsx';
+import GourmetGallery from './pages/ProjectPages/GourmetGallery.jsx';
+import Snake from './pages/ProjectPages/Snake.jsx';
+import SpookSpotters from './pages/ProjectPages/SpookSpotters.jsx';
 
 const App = () => {
   const location = useLocation();
