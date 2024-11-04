4f0863d4fb5eebff4b9b1143bd7c1e85dac6e5ff
Sdg877 <sdrakegill@gmail.com>
2024-11-04 09:06:50 +0000

    Layout component implemented

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import SylviaImage from "../Images/Sylvia.jpeg"; 
 
 const LayOut = () => {
   return (
-    <div>
-      
+    <div className="layout">
+      <img src={SylviaImage} alt="Sylvia Drake-Gill" className="profile-image" />
+      <h1 className="layout-title">Sylvia Drake-Gill</h1>
+      <h2>London-based Software Engineer</h2>
     </div>
-  )
-}
+  );
+};
 
-export default LayOut
+export default LayOut;
