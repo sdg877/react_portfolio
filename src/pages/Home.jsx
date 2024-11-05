76461d37f08aa911f62a4de241b402c651132bd7
Sdg877 <sdrakegill@gmail.com>
2024-11-05 06:57:53 +0000

    Homepage styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import "../App.css"
+import "../App.css";
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <div className="info-card home-container">
       <h1>Welcome to My Portfolio</h1>
       <p>Explore my projects and learn more about me.</p>
       <nav className="home-nav">
-        <ul>
-          <li><Link to="/about">About Me</Link></li>
-          <li><Link to="/education">Education</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/skills">Skills</Link></li>
-        </ul>
+        <div className="nav-links-home">
+          <Link to="/about"><button className="nav-button">About Me</button></Link>
+          <Link to="/education"><button className="nav-button">Education</button></Link>
+          <Link to="/experience"><button className="nav-button">Experience</button></Link>
+          <Link to="/projects"><button className="nav-button">Projects</button></Link>
+          <Link to="/skills"><button className="nav-button">Skills</button></Link>
+        </div>
       </nav>
     </div>
   );
