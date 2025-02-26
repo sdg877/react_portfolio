5dbf0d636aac7784019782f5f2a9e8a85eda69cd
Sdg877 <sdrakegill@gmail.com>
2025-02-26 13:15:06 +0000

    Profile image moved

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import SylviaImage from "../Images/Sylvia.jpeg"; 
 
 const LayOut = () => {
   return (
     <div className="layout">
-      <img src={SylviaImage} alt="Sylvia Drake-Gill" className="profile-image" />
       <h1 className="layout-title">Sylvia Drake-Gill</h1>
       <h2>London-based Software Engineer</h2>
     </div>
