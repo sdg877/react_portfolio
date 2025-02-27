27d97bcae92e4142032ba0e5351240b4c9245711
Sdg877 <sdrakegill@gmail.com>
2025-02-27 07:07:47 +0000

    Hamburger menu implemented

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import "../Styles/Navbar.css"
 
 const NavBar = () => {
   const location = useLocation();
   const currentPath = location.pathname;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const links = [
     { path: '/', label: 'Home' },
@@ -13,16 +15,24 @@ const NavBar = () => {
     { path: '/projects', label: 'Projects' },
     { path: '/skills', label: 'Skills' },
     { path: '/contact', label: 'Contact' }
-
   ];
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className="navbar">
-      <div className="nav-links">
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`line ${isMenuOpen ? 'active' : ''}`}></div>
+        <div className={`line ${isMenuOpen ? 'active' : ''}`}></div>
+        <div className={`line ${isMenuOpen ? 'active' : ''}`}></div>
+      </div>
+      <div className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
         {links
-          .filter(link => link.path !== currentPath) 
+          .filter(link => link.path !== currentPath)
           .map(link => (
-            <Link key={link.path} to={link.path} className="nav-link">
+            <Link key={link.path} to={link.path} className="nav-link" onClick={() => setIsMenuOpen(false)}>
               {link.label}
             </Link>
           ))}
