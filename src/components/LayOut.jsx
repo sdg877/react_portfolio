46291a8e3d713b6cd66488cd111c80dd4a908c49
Sdg877 <sdrakegill@gmail.com>
2025-02-27 08:17:05 +0000

    Update to file name

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Header from './Header';
 
-const LayOut = () => {
+const Layout = () => {
   return (
     <div className="layout">
+       <Header />
       <h1 className="layout-title">Sylvia Drake-Gill</h1>
       <h2>London-based Software Engineer</h2>
     </div>
   );
 };
 
-export default LayOut;
+export default Layout;
