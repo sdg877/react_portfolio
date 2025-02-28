b27ceae3b4f07a47886fd74257afba118fde5960
Sdg877 <sdrakegill@gmail.com>
2025-02-28 07:34:28 +0000

    Remving navbar from home and widget from weather

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './Header';
+import Header from './Header.jsx';
+import NavBar from './Navbar.jsx'; 
 import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
@@ -7,7 +8,8 @@ const Layout = () => {
 
   return (
     <div className="layout">
-      {location.pathname !== '/weather' && <Header />}
+      {location.pathname !== '/weather' && <Header />} 
+      {location.pathname !== '/' && <NavBar />} 
       <h1 className="layout-title">Sylvia Drake-Gill</h1>
       <h2>London-based Software Engineer</h2>
     </div>
