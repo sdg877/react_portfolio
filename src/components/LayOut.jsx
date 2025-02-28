218b1c51bb38f496bc0eb88e16e02d996b317460
Sdg877 <sdrakegill@gmail.com>
2025-02-28 09:51:42 +0000

    Rendering components on correct pages

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import Header from './Header.jsx';
-import NavBar from './Navbar.jsx'; 
+import NavBar from './Navbar.jsx';
+import "../Styles/ComponentStyles.css";
 import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
   const location = useLocation();
+  const isHomePage = location.pathname === '/';
 
   return (
     <div className="layout">
-      {location.pathname !== '/weather' && <Header />} 
-      {location.pathname !== '/' && <NavBar />} 
-      <h1 className="layout-title">Sylvia Drake-Gill</h1>
-      <h2>London-based Software Engineer</h2>
+      <div className={`header-container ${isHomePage ? 'home-page' : ''}`}>
+        {!isHomePage && <NavBar />}
+        {(isHomePage || location.pathname !== '/weather') && (
+          <Header showWeather={true} />
+        )}
+      </div>
+
+      {isHomePage && (
+        <div className="home-content">
+          <h1 className="layout-title">Sylvia Drake-Gill</h1>
+          <h2 className="layout-sub">London-based Software Engineer</h2>
+        </div>
+      )}
     </div>
   );
 };
