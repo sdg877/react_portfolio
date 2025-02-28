55b465eeada88d4982c044bf9c1c40b0a8155644
Sdg877 <sdrakegill@gmail.com>
2025-02-28 06:53:05 +0000

    Header removed from weather page

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className="layout">
-       <Header />
+      {location.pathname !== '/weather' && <Header />}
       <h1 className="layout-title">Sylvia Drake-Gill</h1>
       <h2>London-based Software Engineer</h2>
     </div>
