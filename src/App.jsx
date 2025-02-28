362a3e935225d269d1a8659e8a295aaf252d5aa3
Sdg877 <sdrakegill@gmail.com>
2025-02-28 09:47:43 +0000

    Update to layout of pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useLocation,
 } from "react-router-dom";
 import AboutMe from "./pages/AboutMe.jsx";
 import Education from "./pages/Education.jsx";
@@ -24,149 +23,147 @@ import Layout from "./components/Layout.jsx";
 import Footer from "./components/Footer.jsx";
 
 const App = () => {
-  const location = useLocation();
-
   return (
     <div>
-
-      <Routes>
-      <Route
-          path="/weather"
-          element={
-            <div>
-              <Layout />
-              <Weather />
-            </div>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <div>
-              <Layout />
-              <AboutMe />
-            </div>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <div>
-              <Layout />
-              <Contact />
-            </div>
-          }
-        />
-        <Route
-          path="/experience"
-          element={
-            <div>
-              <Layout />
-              <Experience />
-            </div>
-          }
-        />
-        <Route
-          path="/education"
-          element={
-            <div>
-              <Layout />
-              <Education />
-            </div>
-          }
-        />
-        <Route
-          path="/skills"
-          element={
-            <div>
-              <Layout />
-              <Skills />
-            </div>
-          }
-        />
-        <Route
-          path="/projects"
-          element={
-            <div>
-              <Layout />
-              <Projects />
-            </div>
-          }
-        />
-        <Route
-          path="/projects/GourmetGallery"
-          element={
-            <div>
-              <Layout />
-              <GourmetGallery />
-            </div>
-          }
-        />
-        <Route
-          path="/projects/Snake"
-          element={
-            <div>
-              <Layout />
-              <Snake />
-            </div>
-          }
-        />
-        <Route
-          path="/projects/SpookSpotters"
-          element={
-            <div>
-              <Layout />
-              <SpookSpotters />
-            </div>
-          }
-        />
-        <Route
-          path="/projects/SSS"
-          element={
-            <div>
-              <Layout />
-              <SSS />
-            </div>
-          }
-        />
-        <Route
-          path="/projects/HTJ"
-          element={
-            <div>
-              <Layout />
-              <HTreharneJones />
-            </div>
-          }
-        />
-        <Route
-          path="/projects/InTechJobs"
-          element={
-            <div>
-              <Layout />
-              <InTech />
-            </div>
-          }
-        />
-        <Route
-          path="/projects/ProjectOne"
-          element={
-            <div>
-              <Layout />
-              <ProjectOne />
-            </div>
-          }
-        />
-
-        <Route
-          path="/"
-          element={
-            <div>
-              <Layout />
-              <Home />
-            </div>
-          }
-        />
-      </Routes>
+      <div className="layout-wrapper">
+        <Routes>
+          <Route
+            path="/weather"
+            element={
+              <div>
+                <Layout />
+                <Weather />
+              </div>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <div>
+                <Layout />
+                <AboutMe />
+              </div>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <div>
+                <Layout />
+                <Contact />
+              </div>
+            }
+          />
+          <Route
+            path="/experience"
+            element={
+              <div>
+                <Layout />
+                <Experience />
+              </div>
+            }
+          />
+          <Route
+            path="/education"
+            element={
+              <div>
+                <Layout />
+                <Education />
+              </div>
+            }
+          />
+          <Route
+            path="/skills"
+            element={
+              <div>
+                <Layout />
+                <Skills />
+              </div>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <div>
+                <Layout />
+                <Projects />
+              </div>
+            }
+          />
+          <Route
+            path="/projects/GourmetGallery"
+            element={
+              <div>
+                <Layout />
+                <GourmetGallery />
+              </div>
+            }
+          />
+          <Route
+            path="/projects/Snake"
+            element={
+              <div>
+                <Layout />
+                <Snake />
+              </div>
+            }
+          />
+          <Route
+            path="/projects/SpookSpotters"
+            element={
+              <div>
+                <Layout />
+                <SpookSpotters />
+              </div>
+            }
+          />
+          <Route
+            path="/projects/SSS"
+            element={
+              <div>
+                <Layout />
+                <SSS />
+              </div>
+            }
+          />
+          <Route
+            path="/projects/HTJ"
+            element={
+              <div>
+                <Layout />
+                <HTreharneJones />
+              </div>
+            }
+          />
+          <Route
+            path="/projects/InTechJobs"
+            element={
+              <div>
+                <Layout />
+                <InTech />
+              </div>
+            }
+          />
+          <Route
+            path="/projects/ProjectOne"
+            element={
+              <div>
+                <Layout />
+                <ProjectOne />
+              </div>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <div>
+                <Layout />
+                <Home />
+              </div>
+            }
+          />
+        </Routes>
+      </div>
 
       <Footer />
     </div>
