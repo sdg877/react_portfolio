5a6e8deeda2242e9a115c6ca564d86724fb4420b
Sdg877 <sdrakegill@gmail.com>
2025-03-05 16:48:41 +0000

    Animation added

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import "../Styles/Education.css";
 
 const Education = () => {
   return (
-    <div className="info-card">
-        <h3>Education</h3>
-        <p>
-            <strong>11/2023 - 03/2024
-            <br />General Assembly</strong>
-            <br/>Software Engineering Bootcamp
-            <br/><br/>
-            <strong>2020
-            <br/>Open University</strong>
-            <br/>Science, Maths and Technology access course (equivalent to A-levels)   
-            <br/>Passed with a distinction (95%) - A grade  
-            <br/><br/>
-            <strong>2009
-            <br/>City University</strong>
-            <br/>Human Resources Management Course (Passed)  
-            <br/><br/>
-            <strong>2004 - 2005
-            <br/>City of Westminster College</strong>
-            <br/>Level 3 NVQ in Business Administration  
+    <motion.div 
+      className="info-card"
+      initial={{ opacity: 0, y: 50 }} 
+      animate={{ opacity: 1, y: 0 }} 
+      transition={{ duration: 0.8, ease: "easeOut" }} 
+      whileHover={{ scale: 1.05 }} 
+    >
+      <h3>Education</h3>
+      <p className="education-details">
+        <strong>11/2023 - 03/2024
+        <br />General Assembly</strong>
+        <br />Software Engineering Bootcamp
+        <br /><br />
+        <strong>2020
+        <br />Open University</strong>
+        <br />Science, Maths and Technology access course (equivalent to A-levels)   
+        <br />Passed with a distinction (95%) - A grade  
+        <br /><br />
+        <strong>2009
+        <br />City University</strong>
+        <br />Human Resources Management Course (Passed)  
+        <br /><br />
+        <strong>2004 - 2005
+        <br />City of Westminster College</strong>
+        <br />Level 3 NVQ in Business Administration  
+      </p>
+    </motion.div>
+  );
+};
 
-        </p>
-    </div>
-  )
-}
-
-export default Education
+export default Education;
