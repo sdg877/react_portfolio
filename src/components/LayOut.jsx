8552d1e9257718e4814f4a6fb5b39f4a84809d65
Sdg877 <sdrakegill@gmail.com>
2025-03-06 12:45:06 +0000

    Update to class names for styling

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import Header from './Header.jsx';
-import NavBar from './Navbar.jsx';
+import React, { useState, useEffect } from "react";
+import Header from "./Header.jsx";
+import NavBar from "./Navbar.jsx";
 import "../Styles/ComponentStyles.css";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import lightLogo from "../Images/sdg-dark.png";
+import darkLogo from "../Images/sdg-light.png";
 
 const Layout = () => {
   const location = useLocation();
-  const isHomePage = location.pathname === '/';
+  const isHomePage = location.pathname === "/";
+
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
   return (
-    <div className="layout">
-      <div className={`header-container ${isHomePage ? 'home-page' : ''}`}>
+    <div className={`layout ${theme}`}>
+      <div className={`header-container ${isHomePage ? "home-page" : ""}`}>
         {!isHomePage && <NavBar />}
-        {(isHomePage || location.pathname !== '/weather') && (
-          <Header showWeather={true} />
+        {(isHomePage || location.pathname !== "/weather") && (
+          <Header showWeather={true} toggleTheme={toggleTheme} theme={theme} />
         )}
       </div>
 
       <div className="home-content">
-        <h1 className="layout-title">Sylvia Drake-Gill</h1>
-        <h2 className="layout-sub">London-based Software Engineer</h2>
+        <img
+          src={theme === "light" ? lightLogo : darkLogo}
+          alt="Logo"
+          className="layout-logo"
+        />
+        <h2 className="layout-name">Sylvia Drake-Gill</h2>
+        <h3 className="layout-sub">London-based Software Engineer</h3>
       </div>
     </div>
   );
