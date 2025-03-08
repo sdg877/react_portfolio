c1a8f736c5bef36012bbe04a710b7d4d37129220
Sdg877 <sdrakegill@gmail.com>
2025-03-08 06:27:12 +0000

    Class name for styling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -70,6 +70,7 @@ const Projects = () => {
         initial={{ opacity: 0, y: -30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
+        className="title-project"
       >
         Projects
       </motion.h3>
