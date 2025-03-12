defa302eb5ba8386943529b014322f52d2a018f3
Sdg877 <sdrakegill@gmail.com>
2025-03-12 07:22:28 +0000

    Update to classnames for mobielstylign

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -29,6 +29,7 @@ const Experience = () => {
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
           }}
+          className="details"
         >
           <strong>
             Heather Treharne Jones – Artist Portfolio Website
@@ -47,6 +48,7 @@ const Experience = () => {
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
           }}
+          className="details"
         >
           <strong>
             Student, Software Engineering Bootcamp, General Assembly
@@ -69,6 +71,7 @@ const Experience = () => {
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
           }}
+          className="details"
         >
           Since completing my bootcamp, I have continued to build on my skills by coding a range of projects, learning new languages, and refining my expertise in full-stack development. I completed my first paid project in early 2024 and remain committed to expanding my technical knowledge through hands-on experience and real-world applications.
         </motion.p>
@@ -90,6 +93,7 @@ const Experience = () => {
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
           }}
+          className="details"
         >
           I was responsible for managing a team of three assistants, running the UK office, and supporting up to one hundred employees at the office and one thousand remote employees based across the UK. The main aspects of my role were: health & safety, logistics & facilities management, team supervision and development, and financial management. I also project-managed an office move from start to finish.
         </motion.p>
@@ -99,6 +103,7 @@ const Experience = () => {
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
           }}
+          className="details"
         >
           <strong>
             The Maverick Group, Office Manager
@@ -111,6 +116,7 @@ const Experience = () => {
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
           }}
+          className="details"
         >
           I had one direct report and together we supported 120 people providing administration support and ensuring every aspect of the agency ran smoothly. My duties varied daily but included event planning, compliance, building security, health & safety, the project management of an office expansion and office refurbishment, resource and stock management, and heading up the Corporate Social Responsibility team.
         </motion.p>
@@ -120,6 +126,7 @@ const Experience = () => {
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
           }}
+          className="details"
         >
           <strong>
             British Broadcasting Corporation, Various Team Assistant Positions
@@ -132,6 +139,7 @@ const Experience = () => {
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
           }}
+          className="details"
         >
           I worked across various Creative Marketing and Publicity teams providing admin support and as a Personal Assistant to several Senior Directors. I provided comprehensive diary management, took meeting minutes, and prepared documents as required. I was also responsible for administrating the BBC Pictures site, which provided images of BBC productions for use in the press, and was responsible for the booking of the photographic studio at Television Centre. I provided event management support, including a press event for David Attenborough’s Frozen Planet and Radio 1’s Big Weekend among others.
         </motion.p>
