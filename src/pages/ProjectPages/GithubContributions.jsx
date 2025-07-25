7e6428f3b4b9cc9252530573119f92d05b856de0
Sdg877 <sdrakegill@gmail.com>
2025-07-25 13:16:43 +0100

    Classname for styling purposes.

--- src/pages/ProjectPages/GithubContributions.jsx
+++ src/pages/ProjectPages/GithubContributions.jsx
@@ -57,7 +57,7 @@ const GithubContributions = () => {
   return (
     <div className="info-card-project">
       <h3 className="github-title">My Contributions</h3>
-      <h9>I regularly commit to projects on GitHub, here’s my contribution history.</h9>
+      <h9 className="github-desc">I regularly commit to projects on GitHub, here’s my contribution history.</h9>
       <div className="contribution-calendar">
         {weeks.map((week, i) => (
           <div key={i} className="week-column">
