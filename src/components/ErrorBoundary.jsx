e48fbb31b5b73d98d103079d14b89971aff0b667
Sdg877 <sdrakegill@gmail.com>
2025-11-07 06:14:01 +0000

    Error boundary file added to display downpage if site unavailable.

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import DownPage from "./DownPage";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("Error caught by ErrorBoundary:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <DownPage />;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
