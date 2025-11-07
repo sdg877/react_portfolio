80522e423b8674de3ee385552db1b79e238dfba3
Sdg877 <sdrakegill@gmail.com>
2025-11-07 06:14:35 +0000

    Downpage added to display if site unavailable.

---
+++ src/components/DownPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+const DownPage = () => {
+  return (
+    <div className="down-page">
+      <h1>Site Temporarily Unavailable</h1>
+      <p>
+        Something isn’t loading correctly right now. Please check back soon,
+        we’re working to fix it.
+      </p>
+    </div>
+  );
+};
+
+export default DownPage;
