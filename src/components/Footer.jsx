6bc76ee3b2dfeafb4e4d78ce989b5da12ff89179
Sdg877 <sdrakegill@gmail.com>
2026-03-03 04:51:59 +0000

    Repo link added to footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,15 @@ const Footer = () => {
       <div className="footer">
         <p className="footer-text">
           © 2026 Sylvia Drake-Gill. All rights reserved. Built with React.
+          <br/>
+          <a 
+            href="https://github.com/sdg877/react_portfolio" 
+            target="_blank" 
+            rel="noreferrer"
+            className="custom-link-footer"
+          >
+            View Source
+          </a>
         </p>
       </div>
     </footer>
