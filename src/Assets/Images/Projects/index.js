05fb87b7741e358bcf4b2dd346a4142326bd67e9
Sdg877 <sdrakegill@gmail.com>
2026-03-15 07:03:21 +0000

    Assets file created, project imaages added to index.js file for easy imports, video folder created for CarbonCalc walkthrough.

---
+++ src/Assets/Images/Projects/index.js
@@ -0,0 +1,21 @@
+import AIChatBotImg from "./AIChatBot.png";
+import GourmetGalleryImg from "./GourmetGallery.png";
+import HTreharneJonesImg from "./HTJ.png";
+import InTechJobsImg from "./InTech.png";
+import ProjectOneImg from "./ProjectOne.jpg";
+import SnakeImg from "./Snake.png";
+import SpookSpottersImg from "./SpookSpotters.png";
+import SSSImg from "./SSS.png";
+import CCImg from "./CarbonCalc.png";
+
+export {
+  AIChatBotImg,
+  GourmetGalleryImg,
+  HTreharneJonesImg,
+  InTechJobsImg,
+  ProjectOneImg,
+  SnakeImg,
+  SpookSpottersImg,
+  SSSImg,
+  CCImg
+};
